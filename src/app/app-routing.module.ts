5f417a31616aae946b07655c9c9c808e584de978
Abdelrahman Hegab <abdulrahman.hegab@tensorgraph.io>
2022-02-20 00:16:57 +0200

    first look

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MusicSearchComponent } from './components/music-search/music-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component : HomePageComponent},{path:'music', component :MusicSearchComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
