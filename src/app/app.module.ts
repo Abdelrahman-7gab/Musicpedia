6dfcba10da4c6682b1f6de2ac1b72294b72a4771
Abdelrahman Hegab <abdulrahman.hegab@tensorgraph.io>
2022-06-17 01:04:25 +0200

    added audio player, card design and css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MusicSearchComponent } from './components/music-search/music-search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { SearchResultsPageComponent } from './components/search-results-page/search-results-page.component';
+import { SearchResultsPageComponent } from './pages/search-results-page/search-results-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardComponent } from './components/card/card.component';
+import { RecordingPlayerComponent } from './components/recording-player/recording-player.component';
+import { NgxFitTextModule } from '@pikselin/ngx-fittext';
 
 @NgModule({
   declarations: [
@@ -22,8 +24,9 @@ import { CardComponent } from './components/card/card.component';
     FooterComponent,
     SearchResultsPageComponent,
     CardComponent,
+    RecordingPlayerComponent,
   ],
-  imports: [HttpClientModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule,FormsModule],
+  imports: [HttpClientModule,BrowserModule, AppRoutingModule, BrowserAnimationsModule,FormsModule,NgxFitTextModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/search-results-page/search-results-page.component.ts
+++ src/app/pages/search-results-page/search-results-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { SearchService } from 'src/app/services/search.service';
 
 @Component({
@@ -7,10 +8,17 @@ import { SearchService } from 'src/app/services/search.service';
   styleUrls: ['./search-results-page.component.scss']
 })
 export class SearchResultsPageComponent implements OnInit {
+  playingAudio:string = "";
+  searchResults:BehaviorSubject<any> = new BehaviorSubject<any>({});
 
-  constructor(public searchService:SearchService) { }
+  constructor(private searchService:SearchService) { }
 
   ngOnInit(): void {
+    this.searchResults = this.searchService.getResults();
+  }
+
+  changePlayingSound(uuid:string){
+    this.playingAudio = uuid;
   }
 
 }
