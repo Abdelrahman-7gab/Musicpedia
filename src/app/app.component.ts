2282bcec4774eea74dccee03bda0bd35bdc70746
Abdelrahman Hegab <abdulrahman.hegab@tensorgraph.io>
2022-06-18 12:53:05 +0200

    emitted redundant store calls and more efficent code + some formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SearchService } from './services/search.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +9,6 @@ export class AppComponent {
   title = 'Musicpedia';
 
   constructor(
-    private searchService: SearchService
   ) {
 
   }
--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -2,15 +2,15 @@ import { ActionReducerMap } from "@ngrx/store";
 import searchReducer from "./searchResults/searchResults.store";
 import  playingAudioReducer from "./playingAudio/playingAudio.store";
 import { searchEffects } from "./searchResults/searchResults.effects";
-import trackPageReducer from "./trackPage/trackpage.store";
-import { trackPageEffects } from "./trackPage/trackpage.effects";
+import metadataReducer from "./metadata/metadata.store";
+import { trackPageEffects } from "./metadata/metadata.effects";
 
 
 
 export const reducers:ActionReducerMap<any> = {
     playingAudio:playingAudioReducer,
     searchResults:searchReducer,
-    trackPage:trackPageReducer
+    metadata:metadataReducer
 }
 
 export const effects:any[] = [searchEffects,trackPageEffects];
