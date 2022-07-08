ab4418fe38f53106a0da5446850c4905872a1a08
Abdelrahman Hegab <abdulrahman.hegab@tensorgraph.io>
2022-07-08 05:57:28 +0200

    changes for creating desktop version using tauri

--- src/app/state/searchResults/searchResults.effects.ts
+++ src/app/state/searchResults/searchResults.effects.ts
@@ -1,30 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { map, catchError,of } from 'rxjs';
-import { ICard, TrackAPI } from 'src/app/services/Imusic';
-import { environment } from 'src/environments/environment';
-import { getSearchResults,failedSearch,changeSearchResults } from './searchResults.store';
+import { map, catchError, of } from 'rxjs';
+import { TrackAPI } from 'src/app/services/Imusic';
+import {
+  getSearchResults,
+  failedSearch,
+  changeSearchResults,
+} from './searchResults.store';
 import { switchMap } from 'rxjs';
 import { mapToICard } from 'src/app/services/Imusic';
+import { fetch } from '@tauri-apps/api/http';
+import { from } from 'rxjs';
 
 @Injectable()
 export class searchEffects {
   // results$ = Observable</TrackAPI>
 
-  searchEffect = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(getSearchResults),
-        switchMap(({ payload }) =>
-            this.http.get<TrackAPI>(
-                `${environment.serverUrl}/search/${payload.queryType}/${payload.query}`
-            ).pipe(map((results) => changeSearchResults({results:{...results, data : results.data.map(mapToICard)}})),
-            
-            catchError((error) => of(failedSearch({error:error})))),
-        
+  searchEffect = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getSearchResults),
+      switchMap(({ payload }) =>
+        from(
+          fetch(
+            `https://api.deezer.com/search/${payload.queryType}?q=${payload.query}`
+          )
         )
-      ),
+          .pipe(map((response) => response.data))
+          .pipe(
+            map((results) =>
+              changeSearchResults({
+                results: {
+                  ...(results as TrackAPI),
+                  data: (results as TrackAPI).data.map(mapToICard),
+                },
+              })
+            ),
+
+            catchError((error) => of(failedSearch({ error: error })))
+          )
+      )
+    )
   );
-  constructor(private http: HttpClient, private actions$: Actions) {}
+  constructor(private actions$: Actions) {}
 }
