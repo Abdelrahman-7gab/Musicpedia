8414d0dec5cd49612da923904eedbf309e8a8457
Abdelrahman Hegab <prenshegab@gmail.com>
2023-04-19 02:49:26 +0200

    added audio player, card design and css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { SearchResultsPageComponent } from './components/search-results-page/search-results-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchResultsPageComponent } from './pages/search-results-page/search-results-page.component';
 
 const routes: Routes = [{path:'',component : HomePageComponent},{path:'search', component :SearchResultsPageComponent }];
 
