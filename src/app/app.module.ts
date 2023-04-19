12fed71da0c2cc39927d39a8b4a0e64f65808254
Abdelrahman Hegab <prenshegab@gmail.com>
2023-04-19 02:49:25 +0200

    first look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MusicSearchComponent } from './components/music-search/music-search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicSearchComponent,
     NavbarComponent,
-    HomePageComponent
+    HomePageComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/music-search/music-search.component.ts
+++ src/app/components/music-search/music-search.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-music-search',
   templateUrl: './music-search.component.html',
-  styleUrls: ['./music-search.component.scss']
+  styleUrls: ['./music-search.component.scss'],
 })
 export class MusicSearchComponent implements OnInit {
+  searchParam:string = "track";
 
-  searchResults:any = ["potato","tomato"];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  AorAn(word: string): string {
+    if (
+      word.charAt(0) !== 'a' &&
+      word.charAt(0) !== 'o' &&
+      word.charAt(0) !== 'u' &&
+      word.charAt(0) !== 'u' &&
+      word.charAt(0) !== 'i'
+    )
+      return 'a ' + word;
+
+     return 'an ' + word;
+  }
+
+  setSearchParam(param:string):void{
+    this.searchParam = param;
+  }
+  
+  startSearch(word:string):void{
+    if(word.trim() != "")
+    console.log(word.trim());
   }
 
 }
