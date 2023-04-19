c01b293e5eb2926941e0ba9efeea98b81b654526
Abdelrahman Hegab <prenshegab@gmail.com>
2023-04-19 02:51:26 +0200

    added lyrics ovh api and logic in ngrx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchResultsPageComponent } from './pages/search-results-page/search-results-page.component';
+import { TrackComponent } from './pages/track/track.component';
 
-const routes: Routes = [{path:'',component : HomePageComponent},{path:'search', component :SearchResultsPageComponent }];
+const routes: Routes = [{path:'',component : HomePageComponent},{path:'search', component :SearchResultsPageComponent },{path:'track', component :TrackComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  serverUrl:"http://localhost:3000"
+  serverUrl:"http://localhost:3000",
+  lyricsURL:"https://api.lyrics.ovh/v1"
 };
 
 /*
