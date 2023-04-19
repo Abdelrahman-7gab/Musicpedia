b33dc46fa417b4c9378d2c0f998bedd9c996b3ad
Abdelrahman Hegab <prenshegab@gmail.com>
2023-04-19 02:49:25 +0200

    basic website look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MusicSearchComponent } from './components/music-search/music-search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicSearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
