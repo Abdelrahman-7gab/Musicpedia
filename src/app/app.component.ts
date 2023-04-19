07270ec3218fd010461147012d8ad8e1c0888599
Abdelrahman Hegab <prenshegab@gmail.com>
2023-04-19 02:51:24 +0200

    viewing search titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,24 @@ import { SearchService } from './services/search.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Musicpedia';
 
-  constructor(private route:ActivatedRoute, public searchService:SearchService){
-
-    console.log(this.route);
-
+  constructor(
+    private route: ActivatedRoute,
+    public searchService: SearchService
+  ) {
     this.route.queryParams.subscribe(async (params) => {
-      let searchParam = params['p'];
-      let query = params['q'];
-      let viewType = params['t'];
-      let viewQuery = params['vq'];
+      const searchQuery = params['sq'];
 
-      if(searchParam != null)
-      this.searchService.searchParam = searchParam;
-      if(query != null)
-      this.searchService.query = query;
-      if(viewType != null)
-      this.searchService.viewType = viewType;
-      if(viewQuery != null)
-      this.searchService.viewQuery = viewQuery;
+      this.searchService.searchType = params['st'] || 'track';
+      this.searchService.query = searchQuery || '';
+      this.searchService.viewType = params['vt'] || '';
+      this.searchService.viewQuery = params['vq'] || '';
 
-      
-
-    })
-
+      if (searchQuery != null) this.searchService.getSearchResults();
+    });
   }
 }
